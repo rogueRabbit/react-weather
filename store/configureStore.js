567a93a010f6f095165a147b312496423bf2cd32
pandeng.denley <pandeng.denley@shandagames.com>
2018-08-07 18:29:01 +0800

    初次提交

---
+++ store/configureStore.js
@@ -0,0 +1,23 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk  from 'redux-thunk';
+import reducer from '../reducers';
+
+const createStoreWithMiddleware = compose(
+    applyMiddleware(
+        thunk
+    ),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+)(createStore);
+
+export default function configureStore(initialState){
+    const store = createStoreWithMiddleware(reducer, initialState);
+
+    if (module.hot) {
+        module.hot.accept('../reducers', () => {
+            const nextReducer = require('../reducers');
+            store.replaceReducer(nextReducer)
+        })
+    }
+
+    return store;
+}
